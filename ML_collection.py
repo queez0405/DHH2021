0d2c827213d9e0e0b571724645257c430af91d89
Dong-Hee Kim <queez0405@gmail.com>
2021-10-05 23:04:01 +0900

    Add transformer result

--- ML_collection.py
+++ ML_collection.py
@@ -1,19 +1,26 @@
 import os
+import datetime
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, ExtraTreesRegressor
+import sklearn
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, ExtraTreesRegressor, BaggingRegressor, GradientBoostingRegressor
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.svm import SVR
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from xgboost import XGBRegressor
+
 from sklearn.feature_selection import SelectFromModel
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
 from sklearn.model_selection import KFold
 
 from arguments import get_args
 from tqdm import tqdm
+
+random_state = 1
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -35,30 +42,106 @@ class AverageMeter(object):
 def get_model(args):
     if args.model == 'linear_reg':
         ml_model = LinearRegression()
+        parameters = None
     elif args.model == 'svr_rbf':
         ml_model = SVR(kernel='rbf', C=100, gamma=0.1, epsilon=.1)
+        parameters = None
     elif args.model == 'svr_linear':
         ml_model = SVR(kernel='linear', C=100, gamma='auto')
+        parameters = None
     elif args.model == 'svr_poly':
         ml_model = SVR(kernel='poly', degree=2, C=100, gamma='auto')
+        parameters = None
+    elif args.model == 'bagging':
+        ml_model = BaggingRegressor(base_estimator=SVR(kernel = 'poly', degree=2, C=100, gamma='auto'), n_estimators=10, random_state=random_state)
+        parameters = None
     elif args.model == 'xgboost':
-        ml_model = XGBRegressor(n_estimators=70, learning_rate=0.1, gamma=0, subsample=0.75, colsample_bytree=1, max_depth=7)
+        ml_model = XGBRegressor(booster='gbtree', n_estimators=250, learning_rate=0.08, gamma=0, subsample=0.75, max_depth=3)
+        parameters = {
+                        'n_estimators': [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200, 250],
+                        'max_depth': [None, 2, 3, 4, 5, 6, 7 , 8, 9, 10, 11, 12, 13, 14, 15, 16],
+                        'learning_rate': [0.08, 0.09, 0.1, 0.11, 0.12],
+                        'subsample': [0.25, 0.5, 0.75, 1.0],
+                        'booster': ['gbtree', 'gblinear', 'dart'],
+                        'gamma': [0, 0.001, 0.002, 0.01, 0.002, 0.1, 0.2]
+                    }
     elif args.model == 'adaboost':
-        ml_model = AdaBoostRegressor(n_estimators=40, random_state=0)
+        ml_model = AdaBoostRegressor(learning_rate=1.2, loss='linear', n_estimators=100, random_state=random_state)
+        parameters = {
+                        'n_estimators': [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 200, 250],
+                        'learning_rate': [0.08, 0.09, 0.1, 0.11, 0.12, 0.2, 0.5, 0.7, 1, 1.2, 1.5],
+                        'loss': ['linear', 'square', 'exponential'],
+                    }
     elif args.model == 'random_forest':
-        ml_model = RandomForestRegressor(n_estimators=50, random_state=0)
+        ml_model = RandomForestRegressor(n_estimators=200, max_depth=15, max_features='auto', criterion='mse', bootstrap=True, random_state=random_state)
+        parameters = {
+                        'n_estimators': [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 200, 250],
+                        'max_depth': [None, 2, 3, 4, 5, 6, 7 ,8, 9, 10, 11, 12, 13, 14, 15, 16],
+                        'criterion': ['mse', 'mae'],
+                        'max_features': ['auto', 'sqrt', 'log2'],
+                        'bootstrap': [True, False],                        
+                    }
     elif args.model == 'extra_tree':
-        ml_model = ExtraTreesRegressor(n_estimators=50, random_state=1)
+        ml_model = ExtraTreesRegressor(n_estimators=100, max_depth=14, criterion='mse', max_features='auto', bootstrap='False', random_state=random_state)
+        parameters = {
+                        'n_estimators': [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 200, 250],
+                        'max_depth': [None, 2, 3, 4, 5, 6, 7 ,8, 9, 10, 11, 12, 13, 14, 15, 16],
+                        'criterion': ['mse', 'mae'],
+                        'max_features': ['auto', 'sqrt', 'log2'],
+                        'bootstrap': [True, False],                        
+                    }
+    elif args.model == 'decision_tree':
+        ml_model = DecisionTreeRegressor(criterion = 'mae', max_depth=3, max_features='auto', random_state=random_state)
+        parameters = {
+                        'criterion': ['mse', 'friedman_mse', 'mae', 'poisson'],
+                        'max_depth': [None, 2, 3, 4, 5, 6, 7 ,8, 9, 10, 11, 12, 13, 14, 15, 16],
+                        'max_features': ['auto', 'sqrt', 'log2'],
+                    }
+    elif args.model == 'gradient_boosting':
+        ml_model = GradientBoostingRegressor(n_estimators=250, learning_rate=0.11, max_depth=2, max_features='auto', criterion='friedman_mse', subsample=0.75, random_state=random_state)
+        parameters = {
+                        'n_estimators': [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 200, 250],
+                        'learning_rate': [0.08, 0.09, 0.1, 0.11, 0.12, 0.2, 0.5, 0.7, 1, 1.2, 1.5],
+                        'max_depth': [None, 2, 3, 4, 5, 6, 7 ,8, 9, 10, 11, 12, 13, 14, 15, 16],
+                        'criterion': ['mse', 'friedman_mse', 'mae'],
+                        'subsample': [0.25, 0.5, 0.75, 1.0],
+                        'max_features': ['auto', 'sqrt', 'log2'],         
+                    }
+    
+    if args.running_mode == 'search_param':
+        return ml_model, parameters
+    else:
+        return ml_model
 
-    return ml_model
+sample_imp_regressor = ['gradient_boosting', 'xgboost', 'adaboost', 'random_forest', 'extra_tree']#'decision_tree',
+# sample_imp_regressor = ['xgboost']
 
-sample_imp_regressor = ['adaboost', 'random_forest', 'extra_tree', 'decision_tree', 'bagging', 'gradient_boosting']
-
-def main():
+def get_whole_csv():
     clinical_csv = pd.read_csv('./dataset/Clinical_Variables.csv',index_col=0)
     genetic_csv = pd.read_csv('./dataset/Genetic_alterations.csv',index_col=0)
     surv_time_csv = pd.read_csv('./dataset/Survival_time_event.csv',index_col=0) 
     treat_csv = pd.read_csv('./dataset/Treatment.csv',index_col=0)
+
+    return clinical_csv, genetic_csv, surv_time_csv, treat_csv
+
+def get_treat_csv(is_treat, clinical_csv, genetic_csv, surv_time_csv, treat_csv):
+    treated_clinical_csv = clinical_csv.loc[is_treat,:]
+    treated_genetic_csv = genetic_csv.loc[is_treat,:]
+    treated_surv_time_csv = surv_time_csv.loc[is_treat,:]
+    treated_treat_csv = treat_csv.loc[is_treat,:]
+
+    return treated_clinical_csv, treated_genetic_csv, treated_surv_time_csv, treated_treat_csv
+
+def get_not_treat_csv(is_not_treat, clinical_csv, genetic_csv, surv_time_csv, treat_csv):
+    not_treated_clinical_csv = clinical_csv.loc[is_not_treat,:]
+    not_treated_genetic_csv = genetic_csv.loc[is_not_treat,:]
+    not_treated_surv_time_csv = surv_time_csv.loc[is_not_treat,:]
+    not_treated_treat_csv = treat_csv.loc[is_not_treat,:]
+
+    return not_treated_clinical_csv, not_treated_genetic_csv, not_treated_surv_time_csv, not_treated_treat_csv
+
+def main():
+    clinical_csv, genetic_csv, surv_time_csv, treat_csv = get_whole_csv()
 
     args = get_args()
     
@@ -70,8 +153,6 @@ def main():
         for i in range(np.array(clinical_csv).shape[1]):
             for j in range(len(clinical_csv)):
                 clinical_OH_list[i][j, np.array(clinical_csv)[j,i]] = 1
-            # coords = np.concatenate([np.arange(len(clinical_csv))[np.newaxis,:],np.array(clinical_csv)[:,i][np.newaxis,:]], axis=0).transpose()
-            # clinical_OH_list[i][tuple([tuple(e) for e in coords])] = 1
 
         clinical_csv = clinical_OH_list[0]
         for i in range(1,len(clinical_OH_list)):
@@ -92,49 +173,166 @@ def main():
         surv_time_csv.loc[is_survive & is_not_treat, 'time']  += surv_time_diff_not_treat
 
     if args.treat_type == 'treat_only':
-        clinical_csv = clinical_csv.loc[is_treat,:]
-        genetic_csv = genetic_csv.loc[is_treat,:]
-        surv_time_csv = surv_time_csv.loc[is_treat,:]
-        treat_csv = treat_csv.loc[is_treat,:]
+        clinical_csv, genetic_csv, surv_time_csv, treat_csv = get_treat_csv(is_treat, clinical_csv, genetic_csv, surv_time_csv, treat_csv)
     elif args.treat_type == 'not_treat_only':
-        clinical_csv = clinical_csv.loc[is_not_treat,:]
-        genetic_csv = genetic_csv.loc[is_not_treat,:]
-        surv_time_csv = surv_time_csv.loc[is_not_treat,:]
-        treat_csv = treat_csv.loc[is_not_treat,:]
-
+        clinical_csv, genetic_csv, surv_time_csv, treat_csv = get_not_treat_csv(is_not_treat, clinical_csv, genetic_csv, surv_time_csv, treat_csv)
+        
     if args.treat_type == 'whole':
         x = np.concatenate((genetic_csv,clinical_csv,treat_csv), axis=1)
     else:
         x = np.concatenate((genetic_csv,clinical_csv), axis=1)
+    
     y = np.array(surv_time_csv.loc[:,'time'])
 
-    kfold = KFold(n_splits=10)
-    ml_model = get_model(args)
-
-    MSE = AverageMeter()
+    RMS = AverageMeter()
     MAE = AverageMeter()
     R2 = AverageMeter()
-    importance = []
 
-    for train_idx, test_idx in tqdm(kfold.split(x)):
-        ml_model.fit(x[train_idx], y[train_idx])
+    if args.running_mode == 'show_perform':
+        kfold = KFold(n_splits=10, random_state=random_state, shuffle=True)
+        ml_model = get_model(args)
 
-        y_pred = ml_model.predict(x[test_idx])
+        for train_idx, test_idx in tqdm(kfold.split(x)):
+            ml_model.fit(x[train_idx], y[train_idx])
 
-        MSE.update(mean_squared_error(y_pred, y[test_idx]))
-        MAE.update(mean_absolute_error(y_pred, y[test_idx]))
-        R2.update(r2_score(y_pred, y[test_idx]))
+            y_pred = ml_model.predict(x[test_idx])
 
-        if args.model in sample_imp_regressor:
-            importance.append(ml_model.feature_importances_[:300])
+            RMS.update(mean_squared_error(y_pred, y[test_idx])**0.5)
+            MAE.update(mean_absolute_error(y_pred, y[test_idx]))
+            R2.update(r2_score(y_pred, y[test_idx]))
 
-        # print(f'\nMSE:{mean_squared_error(y_pred, y[test_idx]):.03f}           MAE:{mean_absolute_error(y_pred, y[test_idx]):.03f}')
-    if (args.model in sample_imp_regressor) and args.treat_type == 'not_treat_only':
-        np.save('./not_treat_imp.npy', np.array(importance).mean(0))
-    elif (args.model in sample_imp_regressor) and args.treat_type == 'treat_only':
-        not_treat_imp = np.load('./not_treat_imp.npy')
-        print((np.array(importance).mean(0) - not_treat_imp).argsort()[280:])
-    print(f'MSE:{MSE.avg:.03f}           MAE:{MAE.avg:.03f}             R2:{R2.avg:.03f}')
+        print(f'RMS:{RMS.avg:.03f}           MAE:{MAE.avg:.03f}             R2:{R2.avg:.03f}')
+        print(RMS.avg)
+        print(MAE.avg)
+        print(R2.avg)
+    elif args.running_mode == 'search_param':
+        kfold = KFold(n_splits=10, random_state=random_state, shuffle=True)
+        with open('./result.txt', mode = 'w') as f:
+            f.write(str(datetime.datetime.now())+'\n')
+        for model in sample_imp_regressor:
+            args.model = model
+            ml_model, parameters = get_model(args)
+            if args.search_type == 'grid':
+                grid_search = GridSearchCV(estimator=ml_model,
+                                param_grid=parameters,
+                                cv=kfold,
+                                n_jobs=-1,
+                                scoring='neg_mean_squared_error',
+                                verbose=2
+                                )
+                grid_search.fit(x,y)
+                with open('./result.txt', mode = 'a') as f:
+                    f.write(str(grid_search.best_estimator_)+'\n')
+                    f.write(str(grid_search.best_params_)+'\n')
+                    f.write(str(grid_search.best_score_)+'\n')
+            elif args.search_type == 'random':
+                random_search = RandomizedSearchCV(estimator=ml_model,
+                                param_distributions=parameters,
+                                cv=kfold,
+                                n_jobs=-1,
+                                scoring='neg_mean_squared_error',
+                                verbose=2
+                                )
+                best_score = -1000.
+                while True:
+                    random_search.fit(x,y)
+                    print(random_search.best_params_)
+                    print(random_search.best_score_)
+                    if random_search.best_score_ > best_score:
+                        best_score = random_search.best_score_
+                        with open('./random_result.txt', mode = 'a') as f:
+                            f.write(str(random_search.best_params_)+'\n')
+                            f.write(str(best_score)+'\n')
+                print(random_search.best_estimator_)
+
+    elif args.running_mode == 'select_gene':
+        not_treated_clinical_csv, not_treated_genetic_csv, not_treated_surv_time_csv, not_treated_treat_csv = get_not_treat_csv(is_not_treat, clinical_csv, genetic_csv, surv_time_csv, treat_csv)
+        not_treated_x = np.concatenate((not_treated_genetic_csv,not_treated_clinical_csv), axis=1)
+        not_treated_y = np.array(not_treated_surv_time_csv.loc[:,'time'])
+        not_treated_importance = []
+
+        treated_clinical_csv, treated_genetic_csv, treated_surv_time_csv, treated_treat_csv = get_treat_csv(is_treat, clinical_csv, genetic_csv, surv_time_csv, treat_csv)
+        treated_x = np.concatenate((treated_genetic_csv,treated_clinical_csv), axis=1)
+        treated_y = np.array(treated_surv_time_csv.loc[:,'time'])
+        treated_importance = []
+
+        point_dic = {i:[] for i in range(genetic_csv.shape[1])}
+        weighted_value = []
+
+        for model in sample_imp_regressor:
+            print(model)
+            args.model = model
+            ml_model = get_model(args)
+
+            kfold = KFold(n_splits=10, random_state=random_state, shuffle=True)
+
+            for train_idx, test_idx in tqdm(kfold.split(x)):
+                ml_model.fit(x[train_idx], y[train_idx])
+
+                y_pred = ml_model.predict(x[test_idx])
+
+                RMS.update(mean_squared_error(y_pred, y[test_idx])**0.5)
+                MAE.update(mean_absolute_error(y_pred, y[test_idx]))
+                R2.update(r2_score(y_pred, y[test_idx]))
+            # print(f'RMS:{RMS.avg:.03f}           MAE:{MAE.avg:.03f}             R2:{R2.avg:.03f}')
+            weighted_value.append(RMS.avg)
+
+            RMS.reset()
+            MAE.reset()
+            R2.reset()
+
+            for train_idx, test_idx in tqdm(kfold.split(not_treated_x)):
+                ml_model.fit(not_treated_x[train_idx], not_treated_y[train_idx])
+
+                y_pred = ml_model.predict(not_treated_x[test_idx])
+
+                RMS.update(mean_squared_error(y_pred, not_treated_y[test_idx])**0.5)
+                MAE.update(mean_absolute_error(y_pred, not_treated_y[test_idx]))
+                R2.update(r2_score(y_pred, not_treated_y[test_idx]))
+
+                not_treated_importance.append(ml_model.feature_importances_[:genetic_csv.shape[1]])
+            # print(f'RMS:{RMS.avg:.03f}           MAE:{MAE.avg:.03f}             R2:{R2.avg:.03f}')
+
+            RMS.reset()
+            MAE.reset()
+            R2.reset()
+
+            for train_idx, test_idx in tqdm(kfold.split(treated_x)):
+                ml_model.fit(treated_x[train_idx], treated_y[train_idx])
+
+                y_pred = ml_model.predict(treated_x[test_idx])
+
+                RMS.update(mean_squared_error(y_pred, treated_y[test_idx])**0.5)
+                MAE.update(mean_absolute_error(y_pred, treated_y[test_idx]))
+                R2.update(r2_score(y_pred, not_treated_y[test_idx]))
+
+                treated_importance.append(ml_model.feature_importances_[:genetic_csv.shape[1]])
+            # print(f'RMS:{RMS.avg:.03f}           MAE:{MAE.avg:.03f}             R2:{R2.avg:.03f}')
+
+            importance_ranking = (np.array(treated_importance).mean(0) - np.array(not_treated_importance).mean(0)).argsort()
+            for rank, gene_num in enumerate(importance_ranking):
+                point_dic[gene_num].append(rank+1)
+
+        ##### add transformer result
+        trans_ranking = np.flip(np.array([295,179,81,35,289,2,18,106,261,147,41,161,242,32,290,199,196,162,108,188,95,213,5,17,218,79,243,154,167,206,44,225,192,215,214,165,135,30,72,185,241,42,117,84,132,277,257,25,103,65,291,24,61,151,100,267,92,168,69,238,239,198,78,184,10,193,7,110,264,297,223,89,46,37,229,27,227,43,55,149,115,235,262,153,296,240,31,124,29,15,232,220,285,204,205,200,98,268,148,67,283,128,266,3,66,271,172,203,12,104,251,248,278,157,253,34,109,228,13,233,63,94,221,96,141,234,282,237,20,113,163,51,259,217,116,293,300,52,208,73,284,45,127,255,216,4,265,250,19,142,189,8,236,279,130,83,120,114,107,182,122,299,286,152,134,294,101,36,195,176,133,129,150,270,181,76,99,87,164,180,207,21,269,158,22,276,47,136,209,86,11,57,91,211,93,54,70,77,49,102,6,171,190,210,252,105,131,58,260,186,14,143,38,222,75,62,125,126,219,123,166,224,53,178,155,111,191,26,212,230,48,231,170,275,246,40,39,71,16,256,156,144,201,254,169,183,80,245,272,64,194,292,33,138,173,280,287,288,249,60,85,175,112,281,139,258,146,263,187,140,226,59,90,244,273,298,74,174,97,160,68,56,202,9,82,137,145,50,23,274,247,177,119,197,159,1,88,118,28,121])-1).tolist()
+        trans_rms = 7.227466821670532
+        for rank, gene_num in enumerate(trans_ranking):
+            point_dic[gene_num].append(rank+1)
+
+        weighted_value.append(trans_rms)
+
+        weighted_value = [1/i for i in weighted_value]
+        weighted_value = [float(i)/sum(weighted_value) for i in weighted_value]
+        for i, rankings in enumerate(point_dic.values()):
+            point_dic[i] = np.average(point_dic[i], weights=weighted_value)
+
+        result = sorted(point_dic.items(), key=lambda x:x[1])
+        import pdb;pdb.set_trace()
+
+        print(np.flip(np.array([i for i,_ in result[-30:]])+1).tolist())
+        
+    else:
+        return
 
 if __name__ == '__main__':
     main()
--- arguments.py
+++ arguments.py
@@ -3,18 +3,11 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser('argument for training')
 
-    parser.add_argument('--save_freq', type=int, default=50,
-                        help='save frequency')
-    parser.add_argument('--batch_size', type=int, default=256,
-                        help='batch_size')
-    parser.add_argument('--num_workers', type=int, default=16,
-                        help='num of workers to use')
-    parser.add_argument('--epochs', type=int, default=500,
-                        help='number of training epochs')
-    parser.add_argument('--gpu', default=0, type=int)
-    parser.add_argument('--dropout',type=float, default=0.1)
+    parser.add_argument('--running_mode',type=str, default='show_perform', choices=['show_perform', 'search_param', 'select_gene'])
+    parser.add_argument('--search_type',type=str, default='gird', choices=['grid','random'])
 
-    parser.add_argument('--model',type=str, default='svr_poly', choices=['linear_reg', 'random_forest', 'extra_tree', 'svr_rbf', 'svr_linear', 'svr_poly', 'adaboost', 'xgboost'])
+    parser.add_argument('--model',type=str, default='svr_poly',
+                                            choices=['linear_reg', 'random_forest', 'extra_tree', 'decision_tree', 'bagging', 'gradient_boosting', 'svr_rbf', 'svr_linear', 'svr_poly', 'adaboost', 'xgboost'])
 
     # dataset option
     parser.add_argument('--clinic_var_OH',type=bool, default=False)
@@ -22,19 +15,11 @@ def get_args():
     parser.add_argument('--event_preprocess',type=str, default='same', choices=['none', 'same', 'diff'])
     parser.add_argument('--surv_time_norm',type=bool, default=False)
 
+    # option for grid search
+
     # optimization
-    parser.add_argument('--optimizer',type=str, default='sgd',choices=['sgd','adam'])
     parser.add_argument('--learning_rate', type=float, default=0.001,
                         help='learning rate')
-    parser.add_argument('--lr_decay_epochs', type=str, default='350,400,450',
-                        help='where to decay lr, can be a list')
-    parser.add_argument('--lr_decay_rate', type=float, default=0.1,
-                        help='decay rate for learning rate')
-    parser.add_argument('--weight_decay', type=float, default=1e-4,
-                        help='weight decay')
-    parser.add_argument('--momentum', type=float, default=0.9,
-                        help='momentum')
-
 
     opt = parser.parse_args()
     opt.save_folder = './save/'
